5423efedbc4303f4762c35966e22c30c742d6e2a
Michael D'Agosta <mdagosta@monkeyinferno.com>
2012-01-17 13:46:18 -0800

    Testing out multi-model stuff

--- helloworld/handlers.py
+++ helloworld/handlers.py
@@ -1,16 +1,19 @@
-#!/usr/bin/python
-#
 # Hello World on Hydra
 #
-# Copyright 2012 Michael D'Agosta
+# Copyright 2012 Michael D'Agosta <mdagosta@codebug.com>
+#
+# This program is copyrighted Free Software, and may be used under the
+# terms of the Python License.
 
 import log
 import tornado.web
 import hydra
+import model
 
 class HelloWorld(hydra.Hydra):
   def get(self):
     self.session_start()
+    model.get_session(self.session['id'])
     self.render("templates/index.html")
 
 handler_urls = [
---
+++ helloworld/model.py
@@ -0,0 +1,26 @@
+# Hello World sample data model
+#
+# Copyright 2012 Michael D'Agosta <mdagosta@codebug.com>
+#
+# This program is copyrighted Free Software, and may be used under the
+# terms of the Python License.
+
+import log, model, stdlib, topology
+import hydra
+
+def db_exec(query, *args, **kwargs):
+  return hydra.model.db_exec(query, *args, schema='helloworld', **kwargs)
+
+def db_get(query, *args, **kwargs):
+  return hydra.model.db_get(query, *args, schema='helloworld', **kwargs)
+
+def db_iter(query, *args, **kwargs):
+  return hydra.model.db_iter(query, *args, schema='helloworld', **kwargs)
+
+def db_query(query, *args, **kwargs):
+  return hydra.model.db_query(query, *args, schema='helloworld', **kwargs)
+
+
+def get_session(session_md5):
+    log.warning(session_md5)
+    db_get('SELECT * FROM session WHERE session_md5=%s', session_md5)
