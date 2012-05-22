9d17ebeda8eba57eb6989c4e6809663b4653c32a
Michael D'Agosta <mdagosta@codebug.com>
2012-05-21 18:34:18 -0700

    Change to setuptools for setup.py develop feature.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='hydra',
       version='0.1',
       description='Hydra Tornado utilities',
       author="Michael D'Agosta",
-      author_email='mdagosta@codebug.net',
+      author_email='mdagosta@codebug.com',
       url='http://github.com/mdagosta/hydra',
       packages=['hydra'],
      )
