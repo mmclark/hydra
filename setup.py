cc38fc7022ce1a7b11606c5464eecd1c26ee4565
Michael D'Agosta <mdagosta@codebug.com>
2012-07-17 00:41:37 -0700

    Use distribute

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
-
 from setuptools import setup
-
 setup(
     name = 'hydra',
     version = '0.11',
-    description = 'Hydra Tornado utilities',
     author = "Michael D'Agosta",
     author_email = 'mdagosta@codebug.com',
+    description = 'Hydra Tornado utilities',
     url = 'https://github.com/mdagosta/hydra',
     packages = ['hydra'],
+    install_requires = [
+        'distribute',
+        'tornado == 2.3',
+    ],
     )
