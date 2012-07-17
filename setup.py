976835fda4501f3aed8381917f10bdea56cd7682
Michael D'Agosta <mdagosta@codebug.com>
2012-07-17 00:32:01 -0700

    Version 0.11

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 
 from setuptools import setup
 
-setup(name='hydra',
-      version='0.1',
-      description='Hydra Tornado utilities',
-      author="Michael D'Agosta",
-      author_email='mdagosta@codebug.com',
-      url='http://github.com/mdagosta/hydra',
-      packages=['hydra'],
-     )
+setup(
+    name = 'hydra',
+    version = '0.11',
+    description = 'Hydra Tornado utilities',
+    author = "Michael D'Agosta",
+    author_email = 'mdagosta@codebug.com',
+    url = 'https://github.com/mdagosta/hydra',
+    packages = ['hydra'],
+    )
