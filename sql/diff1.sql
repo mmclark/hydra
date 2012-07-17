96fa94b55aa917fd24edf9a99ed193cf56edc003
Michael D'Agosta <mdagosta@codebug.com>
2012-07-17 02:06:43 -0700

    Move directory up a level

--- sql/diff1.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `session` (
-  `session_md5` varchar(32) NOT NULL,
-  `created` datetime NOT NULL,
-  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `data` text,
-  PRIMARY KEY  (`session_md5`),
-  KEY `modified` (`modified`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
