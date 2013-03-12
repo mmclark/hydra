ff341f5fe36a64897dd482f1bdb5f98551654cc6
Michael D'Agosta <mdagosta@codebug.com>
2013-03-12 07:24:12 +0100

    Increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 setup(
     name = 'hydra',
-    version = '0.19',
+    version = '0.21',
     author = "Michael D'Agosta",
     author_email = 'mdagosta@codebug.com',
     description = 'Hydra Tornado utilities',
